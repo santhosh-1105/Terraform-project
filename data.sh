26d8b09cb2ecc1edef9ea29aa6d7ce4fd3ab0107
santhosh-1105 <128211012+santhosh-1105@users.noreply.github.com>
2023-06-13 17:25:55 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#yum update -y
-#yum install -y httpd.x86_64
-#systemctl start httpd.service
-#systemctl enable httpd.service
-#echo "Hello World from $(hostname -f)" > /var/www/html/index.html
+sudo yum update -y
+sudo yum install -y httpd.x86_64
+sudo systemctl start httpd.service
+sudo systemctl enable httpd.service
+echo "Hello World from $(hostname -f)" > /var/www/html/index.html
 
