bddf5b71b22631df4b2755e560fa665c8e48ede6
santhosh-1105 <128211012+santhosh-1105@users.noreply.github.com>
2023-06-14 11:38:55 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo yum update -y
-sudo yum install -y httpd.x86_64
-sudo systemctl start httpd.service
-sudo systemctl enable httpd.service
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable httpd
 
