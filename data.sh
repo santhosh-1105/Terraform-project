ff875544519829c5ce71e2f7cb2923ad7e532c78
santhosh-1105 <128211012+santhosh-1105@users.noreply.github.com>
2023-06-15 12:49:42 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -6,9 +6,9 @@ sudo systemctl enable httpd
 echo "hello world from $(hostname -f)" > /var/www/html/index.html
 sudo systemctl restart httpd
 
-sudo yum install -y yum-utils shadow-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install terraform
+yum install -y yum-utils shadow-utils
+yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+yum -y install terraform
 terraform init
 terraform apply --auto-approve
 
