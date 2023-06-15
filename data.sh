e746e3ae3209b038b79005eae8b9bff20a582d3b
santhosh-1105 <128211012+santhosh-1105@users.noreply.github.com>
2023-06-15 14:15:28 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -10,5 +10,7 @@ sudo yum install -y yum-utils shadow-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
 sudo yum -y install terraform
 terraform init
+terraform validate
+terraform plan
 terraform apply --auto-approve
 
