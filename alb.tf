ce6ebd9a166518d03908f977f55b716a3a53105f
santhosh-1105 <128211012+santhosh-1105@users.noreply.github.com>
2023-06-16 12:21:29 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -32,14 +32,12 @@ resource "aws_lb_listener" "external-elb" {
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.target-elb.arn
-    health_check {
-    path = "/api/1/resolve/default?path=/service/my-service"
     port = 80
     healthy_threshold = 3
     unhealthy_threshold = 2
     timeout = 2
     interval = 5
     matcher = "200"  # has to be HTTP 200 or fails
-    }
+    
   }
 }
