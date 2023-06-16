9119bd036aaa0700797e729b6564ced3b2bc67b3
santhosh-1105 <128211012+santhosh-1105@users.noreply.github.com>
2023-06-16 10:02:01 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -3,8 +3,6 @@ sudo yum update -y
 sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
-echo "hello world from $(hostname -f)" > /var/www/html/index.html
-sudo systemctl restart httpd
 
 sudo yum install -y yum-utils shadow-utils
 sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
