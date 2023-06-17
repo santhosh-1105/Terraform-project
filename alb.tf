2d9fb56430862288d66d6e4ad2b802deca61c04e
santhosh-1105 <128211012+santhosh-1105@users.noreply.github.com>
2023-06-17 13:02:05 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -9,14 +9,6 @@ resource "aws_lb_target_group" "target-elb" {
   name     = "ALB-TargetGroup"
   protocol = "HTTP"
   vpc_id   = aws_vpc.my-vpc.id
-    health_check {
-    port = 80
-    healthy_threshold = 3
-    unhealthy_threshold = 2
-    timeout = 2
-    interval = 5
-    matcher = "200"  # has to be HTTP 200 or fails
-  }
 }
 resource "aws_lb_target_group_attachment" "attachment" {
   target_group_arn = aws_lb_target_group.target-elb.arn
