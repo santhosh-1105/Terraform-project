46f05b91871c4272d111f5b02cad5db306f85cf2
santhosh-1105 <128211012+santhosh-1105@users.noreply.github.com>
2023-06-19 11:13:08 +0530

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -8,6 +8,7 @@ resource "aws_lb" "external-alb" {
 resource "aws_lb_target_group" "target-elb" {
   name     = "ALB-TargetGroup"
   protocol = "HTTP"
+  port     = 80
   vpc_id   = aws_vpc.my-vpc.id
 }
 resource "aws_lb_target_group_attachment" "attachment" {
