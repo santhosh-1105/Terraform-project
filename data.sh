ba8d0722b61599d8a0d46cda0696d7b3ce03f59c
santhosh-1105 <128211012+santhosh-1105@users.noreply.github.com>
2023-07-10 12:57:36 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -4,5 +4,6 @@ sudo apt install python3
 sudo apt update
 git clone https://github.com/santhosh-1105/car-prediction.git
 sudo apt install flask
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 python3 app.py
+screen -m -d python3 app.py
