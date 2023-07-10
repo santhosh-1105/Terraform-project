4584effc2e52566639c4d3c197f230d82f41a7b7
santhosh-1105 <128211012+santhosh-1105@users.noreply.github.com>
2023-07-10 12:53:16 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install -y httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
-
-
+sudo apt upgrade
+sudo apt install python3
+sudo apt update
+git clone https://github.com/santhosh-1105/car-prediction.git
+sudo apt install flask
+pip install -r requirements.txt
+python3 app.py
